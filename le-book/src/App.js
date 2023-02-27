421d1edfaf0c644c36975a7236ead178f817c261
yelmesaoudim <younes.el-mesaoudi-meftah@etu.u-bordeaux.fr>
2023-02-27 18:01:57 +0100

    Author input problems

--- le-book/src/App.js
+++ le-book/src/App.js
@@ -1,77 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import axios from 'axios';
+import AuthorInput from './authorInput';
+import BookView from './bookView';
 
-// All current search books will be saved here
-let books = [];
+let authorInput = <AuthorInput/>;
 
 function App() {
   return (
     <div className="App">
       <div className="navigation-bar">
         <h1 className="application-name">LeBook</h1>
-        <AuthorInput/>
+        {authorInput}
       </div>
       <main>
+        <div className="search-books">
+        
+        </div>
       </main>
     </div>
   );
-}
-
-class AuthorInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    // Bindings
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    // Set the user input current value
-    this.setState({value: event.target.value});
-
-    // Request
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:" + this.state.value)
-      .then(response => {
-        // Save data
-        // If there are no books
-        if (typeof response.data.items === 'undefined') {
-          books = [];
-        } else {
-          books = response.data.items;
-          console.log(books);
-        }
-      }
-      )
-      .catch(error => {
-        let content = document.querySelector('main');
-        content.innerHTML = "Oups... Il semblerait qu'une erreur se soit produite."
-      }
-      )
-  }
-
-  handleSubmit(event) {
-    // Avoid default behaviour
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" value={this.state.value} placeholder='Auteur' onChange={this.handleChange}/>
-      </form>
-    );
-  }
-}
-
-class BookView extends React.Component {
-  constructor(props) {
-    super(props);
-    
-  }
 }
 
 export default App;
--- le-book/src/authorInput.js
+++ le-book/src/authorInput.js
@@ -1,8 +1,55 @@
+import React from 'react';
+import axios from 'axios';
+import BookView from './bookView';
+
 /**
  * Class which represents the author input for searching books
  */
-class AuthorInput extends React.Component {
+export default class AuthorInput extends React.Component {
     constructor(props) {
-        super(props)
+      super(props);
+      this.state = {value: ''};
+      this.books = [];
+  
+      // Bindings
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
-}
+  
+    handleChange(event) {
+      // Set the user input current value
+      this.setState({value: event.target.value});
+  
+      // Request
+      axios.get("https://www.googleapis.com/books/v1/volumes?q=inauthor:" + this.state.value)
+        .then(response => {
+          // Save data
+          // If there are no books
+          if (typeof response.data.items === 'undefined') {
+            this.books = [];
+          } else {
+            this.books = response.data.items;
+            console.log(this.books)
+          }
+        }
+        )
+        .catch(error => {
+          let content = document.querySelector('main');
+          content.innerHTML = "Oups... Il semblerait qu'une erreur se soit produite: " + error;
+        }
+        )
+    }
+  
+    handleSubmit(event) {
+      // Avoid default behaviour
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} placeholder='Auteur' onChange={this.handleChange}/>
+        </form>
+      );
+    }
+  }
